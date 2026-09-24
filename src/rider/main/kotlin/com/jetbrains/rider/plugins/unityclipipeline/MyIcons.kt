2cfc6927be5427d909eb221f6ef276cd71bc1d96
Fabio William Conceição <messhias@gmail.com>
2026-09-24 20:01:28 +0100

    Add Rider plugin scaffold for Unity CLI pipeline. (#1)
    Normalize setup onto main history at the repo root, drop nested git and build artifacts, and include nuget.org alongside the Rider SDK feed.

---
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/unityclipipeline/MyIcons.kt
@@ -0,0 +1,8 @@
+package com.jetbrains.rider.plugins.unityclipipeline
+
+import com.intellij.openapi.util.IconLoader
+
+object MyIcons {
+    @JvmField
+    val RiderIcon = IconLoader.getIcon("icons/rider.svg", javaClass)
+}
